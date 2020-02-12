bef4d0a5eec2d91afe58418853dbb47ac3b87e85
tdd12 <54655159+tdd12@users.noreply.github.com>
2020-02-12 14:15:55 +1100

    I added dashboards 1-4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardOneComponent} from './dashboard/dashboard-one/dashboard-one.component';
+import {DashboardTwoComponent} from './dashboard/dashboard-two/dashboard-two.component';
+import {DashboardThreeComponent} from './dashboard/dashboard-three/dashboard-three.component';
+import {DashboardFourComponent} from './dashboard/dashboard-four/dashboard-four.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'dashboard-one', pathMatch: 'full'
+  },
+  {
+    path: 'dashboard-one',
+    component: DashboardOneComponent
+  },
+  {
+    path: 'dashboard-two',
+    component: DashboardTwoComponent
+  },
+  {
+    path: 'dashboard-three',
+    component: DashboardThreeComponent
+  },
+  {
+    path: 'dashboard-four',
+    component: DashboardFourComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
